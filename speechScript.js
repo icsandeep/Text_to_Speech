b421126c7d194b60e61290f5c99556c311e2cbcf
icsandeep <sandeep.ichake@gmail.com>
2023-07-29 17:08:22 +0530

    first commit

---
+++ speechScript.js
@@ -0,0 +1,36 @@
+const textarea = document.querySelector("textarea");
+const button = document.querySelector("button");
+let isSpeaking = true;
+
+const textToSpeech = () => {
+  const synth = window.speechSynthesis;
+  const text = textarea.value;
+
+  if (!synth.speaking && text) {
+    const utternace = new SpeechSynthesisUtterance(text);
+    synth.speak(utternace);
+  }
+
+  if (text.length > 50) {
+    if (synth.speaking && isSpeaking) {
+      button.innerHTML = "Pause";
+      synth.resume();
+      isSpeaking = false;
+    } else {
+      button.innerHTML = "Resume";
+      synth.resume();
+      isSpeaking = true;
+    }
+  } else {
+    isSpeaking = false;
+    button.innerText = "Speaking";
+  }
+  setInterval(() => {
+    if (!synth.speaking && !isSpeaking) {
+      isSpeaking = true;
+      button.innerText = "Convert to Speech";
+    }
+  });
+};
+
+button.addEventListener("click", textToSpeech);
